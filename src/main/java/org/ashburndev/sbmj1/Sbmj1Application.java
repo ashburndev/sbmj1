68bf5edffb516677a21978d585cf35a4ac2beb6b
ashburndev <ashburndev@gmail.com>
2019-02-10 07:49:43 -0500

    first commit

---
+++ src/main/java/org/ashburndev/sbmj1/Sbmj1Application.java
@@ -0,0 +1,14 @@
+package org.ashburndev.sbmj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbmj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbmj1Application.class, args);
+	}
+
+}
+
